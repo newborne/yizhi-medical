76b0eb43b8db92da7bc41cd79144c95d444c5dff
newborne <newborne@foxmail.com>
2021-07-05 10:03:47 +0800

    yizhi-medical-back

---
+++ yizhi-medical-back/yizhi-model/src/main/java/com/yizhi/vo/acl/AssignVo.java
@@ -0,0 +1,11 @@
+package com.yizhi.vo.acl;
+
+import lombok.Data;
+
+@Data
+public class AssignVo {
+
+    private Long roleId;
+
+    private Long[] permissionId;
+}
---
+++ yizhi-medical-back/yizhi-model/src/main/java/com/yizhi/vo/order/SignInfoVo.java
@@ -0,0 +1,22 @@
+package com.yizhi.vo.order;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@ApiModel(description = "签名信息")
+public class SignInfoVo  implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@ApiModelProperty(value = "接口地址")
+	private String apiUrl;
+
+	@ApiModelProperty(value = "签名秘钥")
+	private String signKey;
+
+}
+
