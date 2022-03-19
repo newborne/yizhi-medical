14a78f3be80c7fb2ed13f1914a4e0b04b6c6aa9e
newborne <newborne@foxmail.com>
2022-03-19 21:59:31 +0800

    commit

--- yizhi-medical-back/yizhi-common/common-util/src/main/java/com/yizhi/common/util/result/ResultCodeEnum.java
+++ yizhi-medical-back/yizhi-common/common-util/src/main/java/com/yizhi/common/util/result/ResultCodeEnum.java
@@ -44,7 +44,7 @@ public enum ResultCodeEnum {
     private Integer code;
     private String message;
 
-    private ResultCodeEnum(Integer code, String message) {
+    ResultCodeEnum(Integer code, String message) {
         this.code = code;
         this.message = message;
     }
